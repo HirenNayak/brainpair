0d2e32fc30c4af17dca66b42f15dea932675b447
datlaxbro <khr7171@autuni.ac.nz>
2025-05-06 10:33:45 +1200

    recreating the Pomodoro timer. Created the hook for the timer

--- frontend/src/pages/PomodoroTimerApp.js
+++ frontend/src/pages/PomodoroTimerApp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import Button from "../components/Button.js";
-
+import '../style/PomodoroTimerApp.css'
 // Constants
 const POMODORO_TIME = 25 * 60;
 const SHORT_BREAK_TIME = 5 * 60;
@@ -38,3 +38,46 @@ function useTimer(initialTime, onTick,) {
 
     return { time, isRunning, start, pause, reset };
 }
+
+//basic display component
+const TimerDisplay = ({ seconds, mode }) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = String(seconds % 60).padStart(2, "0");
+    return (
+        <div className="timer-display">
+            <h2>{mode.toUpperCase()}</h2>
+            <p style={{ fontSize: "3rem" }}>{`${minutes}:${seconds}`}</p>
+        </div>
+    );
+}
+
+//Basic controls components
+const TimeControls = ({ isRunning, onStart, onStop, onReset, isResetDisabled }) => {
+    <div className="time-controls">
+        { isRunning ? (
+            <button onClick={onStop}>Stop</button>
+        ) : (
+            <button onClick={onStart}>Start</button>
+        )}
+        <Button onClick={onReset} disabled={isResetDisabled}>Reset</Button>
+    </div>
+};
+
+const PomodoroTimerApp = () => {
+    const [mode, setMode] = useState('pomodoro');
+    const [settings] = useState({
+        pomodoro: POMODORO_TIME / 60,
+        shortBreak: SHORT_BREAK_TIME / 60,
+        longBreak: LONG_BREAK_TIME / 60,
+        }
+
+    );
+    const [time, isRunning, start, pause, reset] = useTimer(POMODORO_TIME, (seconds) => {
+        if (seconds === 0) {
+            playSound();
+        }
+        }
+    )
+};
+
+export default PomodoroTimerApp;

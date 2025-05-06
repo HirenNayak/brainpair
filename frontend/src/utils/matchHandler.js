762803fb22fc9615baf520b8364ada15d2a3978f
HirenNayak <nnq1899@autuni.ac.nz>
2025-05-07 01:04:14 +1200

    Create reusable handleSwipe utility to manage swipe and match logic

---
+++ frontend/src/utils/matchHandler.js
@@ -0,0 +1,25 @@
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase-config";
+
+export const handleSwipe = async (currentUser, targetUser, direction) => {
+  const swipeRef = doc(db, "swipes", currentUser.uid);
+  const swipeData = (await getDoc(swipeRef)).data() || {};
+
+  swipeData[targetUser.uid] = direction;
+  await setDoc(swipeRef, swipeData, { merge: true });
+
+  if (direction === "right") {
+    const targetSwipeRef = doc(db, "swipes", targetUser.uid);
+    const targetSwipeDoc = await getDoc(targetSwipeRef);
+    if (targetSwipeDoc.exists()) {
+      const targetSwipes = targetSwipeDoc.data();
+      if (targetSwipes[currentUser.uid] === "right") {
+        const matchId = [currentUser.uid, targetUser.uid].sort().join("_");
+        await setDoc(doc(db, "matches", matchId), {
+          users: [currentUser.uid, targetUser.uid],
+          timestamp: new Date(),
+        });
+      }
+    }
+  }
+};

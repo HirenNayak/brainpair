971899c18c874b0a0c58b4d6b3faa08f20a81104
HirenNayak <nnq1899@autuni.ac.nz>
2025-05-06 15:23:10 +1200

    Add route for ImageUploadPage to App.js routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfileSetupPage from './pages/ProfileSetupPage';
+import ImageUploadPage from './pages/ImageUploadPage'; 
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/setup" element={<ProfileSetupPage />} />
+        <Route path="/upload" element={<ImageUploadPage />} /> 
       </Routes>
     </Router>
   );

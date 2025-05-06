ba5c709fe7e6a7841c4922b638d712cae2fc97bc
HirenNayak <nnq1899@autuni.ac.nz>
2025-05-07 11:22:44 +1200

    Add app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfileSetupPage from './pages/ProfileSetupPage';
 import ImageUploadPage from './pages/ImageUploadPage'; 
+import DashboardPage from "./pages/DashboardPage";
+ import MatchesPage from "./pages/MatchesPage";
+ import ConnectionsPage from "./pages/ConnectionsPage";
+ import ChatPage from './pages/ChatPage';
 import Pomodoro from './pages/PomodoroTimerApp';
 import UserProfileSettings from "./pages/UserProfileSettings";
 
@@ -17,6 +21,11 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/setup" element={<ProfileSetupPage />} />
         <Route path="/upload" element={<ImageUploadPage />} /> 
+        <Route path="/dashboard" element={<DashboardPage />} />
+         <Route path="/pomodoro" element={<Pomodoro />} /> 
+         <Route path="/matches" element={<MatchesPage />} /> 
+         <Route path="/connections" element={<ConnectionsPage />} />
+         <Route path="/chat/:userId" element={<ChatPage />} />
         <Route path="/pomodoro" element={<Pomodoro />} />
         <Route path="/settings" element={<UserProfileSettings />} />
       </Routes>

87faee41bd6797ddca1dee7fa814343712f61e8b
HirenNayak <nnq1899@autuni.ac.nz>
2025-05-07 21:46:39 +1200

    chore: integrate react-toastify and add ToastContainer to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfileSetupPage from './pages/ProfileSetupPage';
 import ImageUploadPage from './pages/ImageUploadPage'; 
 import DashboardPage from "./pages/DashboardPage";
- import MatchesPage from "./pages/MatchesPage";
- import ConnectionsPage from "./pages/ConnectionsPage";
- import ChatPage from './pages/ChatPage';
+import MatchesPage from "./pages/MatchesPage";
+import ConnectionsPage from "./pages/ConnectionsPage";
+import ChatPage from './pages/ChatPage';
 import Pomodoro from './pages/PomodoroTimerApp';
 import UserProfileSettings from "./pages/UserProfileSettings";
-import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <Router>
+      <ToastContainer position="top-right" autoClose={3000} />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -23,13 +26,11 @@ function App() {
         <Route path="/setup" element={<ProfileSetupPage />} />
         <Route path="/upload" element={<ImageUploadPage />} /> 
         <Route path="/dashboard" element={<DashboardPage />} />
-         <Route path="/pomodoro" element={<Pomodoro />} /> 
-         <Route path="/matches" element={<MatchesPage />} /> 
-         <Route path="/connections" element={<ConnectionsPage />} />
-         <Route path="/chat/:userId" element={<ChatPage />} />
-        <Route path="/pomodoro" element={<Pomodoro />} />
+        <Route path="/pomodoro" element={<Pomodoro />} /> 
+        <Route path="/matches" element={<MatchesPage />} /> 
+        <Route path="/connections" element={<ConnectionsPage />} />
+        <Route path="/chat/:userId" element={<ChatPage />} />
         <Route path="/settings" element={<UserProfileSettings />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
